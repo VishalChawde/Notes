f510bb59b313329d6eb75bc6fd0d6abdb5149ed1
VishalChawde <Vishal.Chawde@ril.com>
2025-06-16 16:18:58 +0530

    hosting commit

--- m_modules/db.js
+++ m_modules/db.js
@@ -1,10 +1,27 @@
+// const mongoose = require('mongoose');
+// const config = require('../config/config.json')
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(config.MONGO_URI);
+//     console.log('MongoDB Connected');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
-const config = require('../config/config.json')
+const config = require('../config/config.json');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.MONGO_URI);
-    console.log('MongoDB Connected');
+    await mongoose.connect(config.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected to Atlas');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
